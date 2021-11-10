9c1eef82b76ee2575634fead7620b87a18c04841
Ytalo Sauham <ytalo.sauhan@hotmail.com>
2021-11-10 06:36:57 -0300

    Table adicionada

--- pages/api/produtos/[id].js
+++ pages/api/produtos/[id].js
@@ -5,13 +5,27 @@ export default function handle(req,res){
     if(req.method==="GET"){
         GETbyID(req,res)
         }
+    if(req.method==="DELETE"){
+            DELETEbyID(req,res)
+            }
 
     
     function GETbyID(req,res){
-        const id=req.query.id
+       
         db('produto')
         .select('codigo', 'descricao', 'marca','tamanho','quantidade','precocusto','precovenda')
-        .where('codigo',id)
+        .where('codigo',req.query.id)
+        .then(produtos => res.json(produtos))
+        .catch(err => res.status(500).send(err))
+             
+    }
+
+
+    function DELETEbyID(req,res){
+     
+        db('produto')
+        .where('codigo',req.query.id)
+        .del()
         .then(produtos => res.json(produtos))
         .catch(err => res.status(500).send(err))
              

9059cb1b0c8a2400ad8bb6e41bdbfbd981483651
Ytalo Sauham <ytalo.sauhan@hotmail.com>
2021-11-24 01:09:28 -0300

    Deploy sem variaveis de habiente

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ const knex = require('knex')({
     client: 'mysql2',
     connection: {
       host : '127.0.0.1',
-      port: process.env.DATABASE_PORT,
-      user : process.env.DATABASE_USER,
-      password :process.env.DATABASE_PASSWORD,
+      port: 3306,
+      user : root,
+      password :'',
       database : 'produto'
     }
   });
